b0646d705b96f52d0b5f878478a7986aa4e1b7de
Ian Rainford <irainford@linz.govt.nz>
2022-04-29 13:21:07 +1200

    Typos

--- lib/html-pdf-lambda-cdk-stack.ts
+++ lib/html-pdf-lambda-cdk-stack.ts
@@ -1,13 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 
 export class HtmlPdfLambdaCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const api = new apigateway.RestApi(this, 'api', {
+    const api = new apiGateway.RestApi(this, 'api', {
       description: 'API gateway for PDF creator',
       deployOptions: {
         stageName: 'dev',
@@ -25,7 +25,7 @@ export class HtmlPdfLambdaCdkStack extends cdk.Stack {
         allowOrigins: ['*'],
       },
     });
-    const createPdfLambbda = new NodejsFunction(this, 'create-pdf', {
+    const createPdfLambda = new NodejsFunction(this, 'create-pdf', {
       memorySize: 1024,
       timeout: cdk.Duration.seconds(20),
       runtime: lambda.Runtime.NODEJS_14_X,
@@ -40,7 +40,7 @@ export class HtmlPdfLambdaCdkStack extends cdk.Stack {
     const createPdf = api.root.addResource('create-pdf');
     createPdf.addMethod(
       'POST',
-      new apigateway.LambdaIntegration(createPdfLambbda, { proxy: true }),
+      new apiGateway.LambdaIntegration(createPdfLambda, { proxy: true }),
     );
 
     new cdk.CfnOutput(this, 'apiUrl', { value: api.url });
